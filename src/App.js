323937830a66ffe73d39fb4ee5925960ee618452
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-07-13 17:47:09 +0530

    world-clock

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Stopwatch from './components/Stopwatch';
+import Countdown from './components/Countdown';
+
+class App extends Component {
+  state = {
+    stopwatch: false,
+    countdown: false,
+  };
+  close = (key) => {
+    this.setState({ [key]: false });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">🚀 Timers 🚀</div>
+        <div className="Timers">
+          {this.state.stopwatch ? (
+            <Stopwatch close={this.close} />
+          ) : (
+            <button onClick={() => this.setState({ stopwatch: true })}>
+              Show Stopwatch
+            </button>
+          )}
+          {this.state.countdown ? (
+            <Countdown close={this.close} />
+          ) : (
+            <button onClick={() => this.setState({ countdown: true })}>
+              Show Countdown
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
